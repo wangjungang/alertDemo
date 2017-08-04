96bef3deca5ed15045f14689d5a6b2dbc7d8a0a3
wangjungang1994 <1499370499@qq.com>
2017-08-04 13:04:35 +0800

    addmore

--- alertDemo/ViewController.m
+++ alertDemo/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "ActionSheetView.h"
 @interface ViewController ()
 
 @end
@@ -25,5 +25,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)btn0click:(id)sender {
+    NSArray *titlearr = @[@"微信朋友圈",@"微信好友",@"微信朋友圈"];
+    NSArray *imageArr = @[@"wechatquan",@"wechat",@"tcentQQ"];
+    
+    ActionSheetView *actionsheet = [[ActionSheetView alloc] initWithShareHeadOprationWith:titlearr andImageArry:imageArr andProTitle:@"测试" and:ShowTypeIsShareStyle];
+    [actionsheet setBtnClick:^(NSInteger btnTag) {
+        NSLog(@"\n点击第几个====%ld\n当前选中的按钮title====%@",btnTag,titlearr[btnTag]);
+    }];
+    [[UIApplication sharedApplication].keyWindow addSubview:actionsheet];
+
+}
+- (IBAction)btn1click:(id)sender {
+    NSArray *titlearr = @[@"微信朋友圈",@"微信好友",@"微信朋友圈",@"微信好友"];
+    
+    ActionSheetView *actionsheet = [[ActionSheetView alloc] initWithShareHeadOprationWith:titlearr andImageArry:@[] andProTitle:@"" and:ShowTypeIsActionSheetStyle];
+    [actionsheet setBtnClick:^(NSInteger btnTag) {
+        NSLog(@"\n点击第几行====%ld\n当前选中的按钮title====%@",btnTag,titlearr[btnTag]);
+    }];
+    [[UIApplication sharedApplication].keyWindow addSubview:actionsheet];
+}
 
 @end
